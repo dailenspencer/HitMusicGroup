eacd7831e970df9c5c850d7f5a13d460656210a2
Dailen Spencer <dailenspencer@gmail.com>
2016-07-21 13:03:33 -0700

    [Fix] Heroku Deployment

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ var server = app.listen(8083, function() {
     var host = server.address().address
     var port = server.address().port
 
-    console.log("Example app listening at http://%s:%s", host, port)
+    console.log("HitMusicGroup app listening at port", host, port)
 
 })
 
