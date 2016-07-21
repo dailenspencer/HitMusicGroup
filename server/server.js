5a48a2c4f914113eede8926e02a8b60c3680429b
Dailen Spencer <dailenspencer@gmail.com>
2016-07-21 10:23:46 -0700

    [Fix] heroku deployement

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var path = require('path');
 app = express();
-app.use(express.static('../public'));
+app.use(express.static(path.join(__dirname,'../public/')));
 
 app.use('/bower_components',  express.static(__dirname + '../public/bower_components'));
 
 
 app.get('*', function(req,res){
-	res.sendFile(path.resolve('../public','index.html'));
+	console.log('__dirname is : ',__dirname)
+	res.sendFile(path.join(__dirname,'../public/index.html'));
 })
 
 var server = app.listen(8083, function() {
