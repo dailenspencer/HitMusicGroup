d3c91bd80e2b93ab1e8de51350efeb520e87a2cb
Dailen Spencer <dailenspencer@gmail.com>
2016-07-22 11:40:54 -0700

    [Fix] Heroku Deploy Fix

--- server/server.js
+++ server/server.js
@@ -3,7 +3,6 @@ var path = require('path');
 app = express();
 app.use(express.static(path.join(__dirname,'../public/')));
 
-app.use('/bower_components',  express.static(__dirname + '../public/bower_components'));
 
 
 app.get('*', function(req,res){
